eb228bff7c51f0e31a2a91f98ddfd61ef2e5c55f
Juan Ignacio <juanirmariani@gmail.com>
2025-02-11 15:22:30 -0300

    First Commit - Create dependencies and firsts files

---
+++ src/main/java/ozoriani/empleomadrynbackend/EmpleoMadrynBackendApplication.java
@@ -0,0 +1,13 @@
+package ozoriani.empleomadrynbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleoMadrynBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpleoMadrynBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ozoriani/empleomadrynbackend/EmpleoMadrynBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ozoriani.empleomadrynbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleoMadrynBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
